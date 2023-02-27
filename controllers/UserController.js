8726494195749d21dceedccee269af9eaf5b5ff3
Ivo Jose <ivojmgs19@gmail.com>
2023-02-27 11:15:33 -0300

    Validation fields

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,20 @@ class UserController {
 
     async create(req,res) {
         console.log(req.body);
+        let {name,email,password,role} = req.body;
+        
+        //Improving data for validation
+        if(name != undefined && email != undefined && password != undefined) {
+            name = name.trim();
+            email = email.trim();
+            password = password.trim(); 
+        }
+        
+        if(name == undefined || name.length > 3 || email == undefined  || email.length == 0 || password == undefined || password.length > 3 || role == undefined) {
+            res.status(400);
+            res.json({err:'Null or incorrectly filled fields are invalid'}); 
+            return;
+        }
         res.send('Getting the request body');
     }
 }
--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.use(bodyParser.json())
 app.use('/', router);
 
 //Server
-app.listen(8080,() => {
+app.listen(8888,() => {
     console.log('Running server');
 }) 
