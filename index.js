d318049b782f5309d0e416037fe567a84d70dd85
Ivo Goncalves <ivojmgs19@gmail.com>
2023-03-30 09:27:37 -0300

    Adding and configuring CORS

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //Imports libs
 let bodyParser = require('body-parser');
 let express = require('express');
+let cors = require('cors');
 //Others imports
 let router = require('./routes/routes');
 //Instances
 let app = express();
+
+
+//Setting CORS
+app.use(cors());
 
 //Configuring express with bodyParse
 // parse application/x-www-form-urlencoded
